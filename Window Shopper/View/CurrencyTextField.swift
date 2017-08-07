133dccb2f803c913d338d880f05595207c04015c
Ryan Loera <rloera@csumb.edu>
2017-08-06 22:11:23 -0400

    created custom view and updated main storyboard

---
+++ Window Shopper/View/CurrencyTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyTextField.swift
+//  Window Shopper
+//
+//  Created by RyLo on 8/6/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.43)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
